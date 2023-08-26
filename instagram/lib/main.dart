bdd4710e04d68663aed6efd857ad17d423e29bf4
Minsik Kim <ppp37686@gmail.com>
2023-08-26 22:05:09 +0900

    Init

---
+++ instagram/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('instagram')),
+      body: Text('asdf'),
+    );
+  }
+}
